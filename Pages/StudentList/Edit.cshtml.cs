cff71b65e717c257582360a07973c03524738fbe
Your Name <you@example.com>
2018-12-31 23:58:56 +0600

    edit

---
+++ Pages/StudentList/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using StudentRazorPage.Model;
+
+namespace StudentRazorPage.Pages.StudentList
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db){
+            _db = db;
+        }
+
+        [BindProperty]
+        public Student Student{get;set;}
+        public void OnGet(int id)
+        {
+            Student = _db.students.Find(id);
+        }
+
+       public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var stdFromDb = _db.students.Find(Student.Id);
+                stdFromDb.Name = Student.Name;
+                stdFromDb.Class = Student.Class;
+                stdFromDb.address = Student.address;
+                stdFromDb.PhoneNumber = Student.PhoneNumber;
+                
+                await _db.SaveChangesAsync();
+                //Message = "Book has been updated successfully";
+
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
+        }
+    }
+}
